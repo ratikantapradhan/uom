1c86863f0979da636f86bf36e25a0d0e2d740c51
RATIKANTA PRADHAN <RATIKANTA PRADHAN@RATIKANTA-PC>
2018-05-10 00:41:46 +0530

    story#3456

--- src/com/nt/test/UomTest.java
+++ src/com/nt/test/UomTest.java
@@ -4,4 +4,27 @@ public class UomTest {
 private String ename;
 private String job;
 private String job1;
+public String getEname() {
+	return ename;
+}
+public void setEname(String ename) {
+	this.ename = ename;
+}
+public String getJob() {
+	return job;
+}
+public void setJob(String job) {
+	this.job = job;
+}
+public String getJob1() {
+	return job1;
+}
+public void setJob1(String job1) {
+	this.job1 = job1;
+}
+@Override
+public String toString() {
+	return "UomTest [ename=" + ename + ", job=" + job + ", job1=" + job1 + "]";
+}
+
 }
